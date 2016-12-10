4ea7e6810f0205d45301a3bd7f36d08885f22ac5
Christy O'Reilly <christy@oreills.co.uk>
2016-12-10 23:30:45 +0000

    Add unset command

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import argparse
 from json import load
 import os
 import subprocess
@@ -12,7 +13,7 @@ CONFIG = load(open(CONFIG_PATH))
 DND_SECS = 60 * 60
 
 
-def dnd_phone(config):
+def dnd_phone(config, unset=False):
     if not config.get('ANDROID_API_KEY'):
         return
 
@@ -20,26 +21,34 @@ def dnd_phone(config):
 
     registration_id = config["ANDROID_INSTANCE_ID"]
 
-    data_message = {
-        "action": "start_dnd",
-        "dnd_secs": str(DND_SECS),
-    }
+    if unset:
+        data_message = {
+            "action": "stop_dnd",
+        }
+    else:
+        data_message = {
+            "action": "start_dnd",
+            "dnd_secs": str(DND_SECS),
+        }
 
     result = push_service.notify_single_device(
         registration_id=registration_id, data_message=data_message)
 
 
-def dnd_slack(config):
+def dnd_slack(config, unset=False):
     if not config.get('SLACK_API_TOKEN'):
         return
 
     sc = SlackClient(config['SLACK_API_TOKEN'])
 
-    sc.api_call("dnd.setSnooze", num_minutes=DND_SECS / 60)
+    if unset:
+        sc.api_call("dnd.endSnooze")
+    else:
+        sc.api_call("dnd.setSnooze", num_minutes=DND_SECS / 60)
 
 
-def open_brain_fm_focus(config):
-    if not config.get('BRAIN_FM'):
+def open_brain_fm_focus(config, unset=False):
+    if unset or not config.get('BRAIN_FM'):
         return
 
     devnull = open(os.devnull, 'w')
@@ -48,6 +57,11 @@ def open_brain_fm_focus(config):
 
 
 if __name__ == '__main__':
-    dnd_phone(CONFIG)
-    dnd_slack(CONFIG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--unset', nargs='?', const=True, default=False)
+
+    args = parser.parse_args()
+
+    dnd_phone(CONFIG, unset=args.unset)
+    dnd_slack(CONFIG, unset=args.unset)
     open_brain_fm_focus(CONFIG)
