d1fc3ddc3170045862381de61c06016aaedd9b94
Christy O'Reilly <christy@oreills.co.uk>
2016-12-10 23:01:36 +0000

    Modularise code more

--- __init__.py
+++ __init__.py
@@ -4,26 +4,39 @@ import os
 from pyfcm import FCMNotification
 from slackclient import SlackClient
 
-
 CONFIG_PATH = '~/.zephzone'
 CONFIG_PATH = os.path.expanduser(CONFIG_PATH)
-
-config = load(open(CONFIG_PATH))
+CONFIG = load(open(CONFIG_PATH))
 
 DND_SECS = 60 * 60
 
-push_service = FCMNotification(api_key=config["ANDROID_API_KEY"])
 
-registration_id = config["ANDROID_INSTANCE_ID"]
+def dnd_phone(config):
+    if not config.get('ANDROID_API_KEY'):
+        return
 
-data_message = {
-    "action" : "start_dnd",
-    "dnd_secs" : str(DND_SECS),
-}
+    push_service = FCMNotification(api_key=config["ANDROID_API_KEY"])
 
-result = push_service.notify_single_device(registration_id=registration_id, data_message=data_message)
+    registration_id = config["ANDROID_INSTANCE_ID"]
 
-sc = SlackClient(config['SLACK_API_TOKEN'])
+    data_message = {
+        "action": "start_dnd",
+        "dnd_secs": str(DND_SECS),
+    }
 
-sc.api_call(
-    "dnd.setSnooze", num_minutes=DND_SECS/60)
+    result = push_service.notify_single_device(
+        registration_id=registration_id, data_message=data_message)
+
+
+def dnd_slack(config):
+    if not config.get('SLACK_API_TOKEN'):
+        return
+
+    sc = SlackClient(config['SLACK_API_TOKEN'])
+
+    sc.api_call("dnd.setSnooze", num_minutes=DND_SECS / 60)
+
+
+if __name__ == '__main__':
+    dnd_phone(CONFIG)
+    dnd_slack(CONFIG)
